4fb3594b7b7d2975d2ebd6f7556abbeaeb52be09
Manuel Brito <manuelcidbrito@tecnico.ulisboa.pt>
2026-01-15 00:53:14 +0100

    only .sh that reall works for now

--- launch_sim.sh
+++ launch_sim.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 gnome-terminal --window \
-	--tab --title="QGroundControl" \
-	--command="bash -c 'cd ~/Downloads/ && ./QGroundControl-x86_64.AppImage;exec bash'"\
-	\
 	--tab --title="Gazebo" \
-	--command="bash -c 'echo \"Waiting 3s for QGC...\"; sleep 3; gz sim -v4 -r swan_k1_hwing_runway.sdf; exec bash'" \
+	--command="bash -c 'gz sim -v4 -r swan_k1_hwing_runway.sdf;exec bash'"\
 	\
  	--tab --title="ArduPilot SITL" \
- 	--command="bash -c 'cd \"$HOME/ardupilot/\"&& ./waf configure --board sitl &&./waf plane && cd ArduPlane/ && sim_vehicle.py -v ArduPlane --model JSON --add-param-file=$HOME/SITL_Models/Gazebo/config/swan_k1_hwing.param --console --map; exec bash'"
-
+ 	--command="bash -c 'sim_vehicle.py -v ArduPlane --model JSON --add-param-file=$HOME/SITL_Models/Gazebo/config/swan_k1_hwing.param --console --map;exec bash'"
